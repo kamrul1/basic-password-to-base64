fb1eb1283239c7930a4d1416827f75fdea746824
kamrul i <to.kamrul@outlook.com>
2021-04-22 02:34:16 +0100

    toastr added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Jumbotron, Button, Container } from "react-bootstrap";
 import { FaGithub } from "react-icons/fa";
 import AlertCopy from "./components/AlertCopy";
+import { ToastProvider } from 'react-toast-notifications';
 const { default: RequestForm } = require("./components/RequestForm");
 
 
@@ -21,25 +22,26 @@ function App() {
 
 
   return (
-    <div className="App">
-      <>
-        <Jumbotron>
-        <Container style={rightCSS}>
-            <h4> Makes Basic Authentication Header</h4>
-            <a href="https://github.com/kamrul1/basic-password-to-base64" target="_blank" rel="noreferrer"><Button variant="dark"><FaGithub/></Button></a>
-        </Container>
-          {
-            response !== '' && (
-              <><br/>
-                <AlertCopy response={response} />
-              </>
-            )
-          }
-        </Jumbotron>
-        <RequestForm encodedValue={updateUI} />
-
-      </>
-    </div>
+    <ToastProvider>
+      <div className="App">
+        <>
+          <Jumbotron>
+            <Container style={rightCSS}>
+              <h4> Makes Basic Authentication Header</h4>
+              <a href="https://github.com/kamrul1/basic-password-to-base64" target="_blank" rel="noreferrer"><Button variant="dark"><FaGithub /></Button></a>
+            </Container>
+            {
+              response !== '' && (
+                <><br />
+                  <AlertCopy response={response} />
+                </>
+              )
+            }
+          </Jumbotron>
+          <RequestForm encodedValue={updateUI} />
+        </>
+      </div>
+    </ToastProvider>
   );
 }
 
--- src/components/AlertCopy.js
+++ src/components/AlertCopy.js
@@ -1,29 +1,42 @@
 import { useState } from "react";
-import { Alert} from "react-bootstrap";
-import {FiCopy} from "react-icons/fi";
+import { Alert } from "react-bootstrap";
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Tooltip from 'react-bootstrap/Tooltip'
+import { FiCopy } from "react-icons/fi";
+import { useToasts } from 'react-toast-notifications'
 
-const AlertCopy = ({response}) => {
+const AlertCopy = ({ response }) => {
 
-    const [alertVarientColor, setAlertVarientColor] = useState('success')
+    const [alertVarientColor, setAlertVarientColor] = useState('success');
+    const { addToast } = useToasts();
 
-    const handleClick = ()=>{
+    const handleClick = () => {
         navigator.clipboard.writeText(response)
         setAlertVarientColor("light")
+
+        addToast("copied to clipboard", {
+            appearance: 'info',
+            autoDismiss: true,
+        });
         setTimeout(() => {
             setAlertVarientColor("success")
         }, 300);
     }
 
-    
+
 
     return (
         <>
-        <Alert 
-            variant={alertVarientColor} 
-            style={{display:"flex", justifyContent:"space-between"}}
+            <Alert
+                variant={alertVarientColor}
+                style={{ display: "flex", justifyContent: "space-between" }}
             >
-                {response}<FiCopy onClick={handleClick}/>
-        </Alert>
+                {response}
+                <OverlayTrigger placement="bottom" 
+                    overlay={<Tooltip id={`tooltip-bottom`}>copy</Tooltip>}>
+                        <FiCopy onClick={handleClick} />
+                </OverlayTrigger>
+            </Alert>
         </>
     )
 }
