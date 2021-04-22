9d00644b6d2f84a0d781863d62b37a520672cb78
kamrul i <to.kamrul@outlook.com>
2021-04-22 02:44:09 +0100

    code clean up

--- my_functions/base64Encode.js
+++ my_functions/base64Encode.js
@@ -3,23 +3,15 @@ var utf8 = require('utf8');
 
 exports.handler = async function (event, context) {
 
-    // let payloadBody = {
-    //     message: "",
-    //     otherMsg:""
-    // }
-
     let response = {
         statusCode:500,
         body: "Not processed, Netlify function is not working"
     }
 
-    // console.log(event.httpMethod);
-
     if(event.httpMethod!=="POST"){
         return {...response, statusCode: 400, body:"This function only works via a post request"}
     };
 
-    console.log(event.body);
 
     if(event.body===undefined){
         return  {...response, statusCode:400, body: "Payload is empty"}
@@ -27,23 +19,12 @@ exports.handler = async function (event, context) {
 
     const {username, password} = JSON.parse(event.body);
 
-    // let base64Script = base64.encode(userName+":"+password);
-
     let stringToEncode = utf8.encode(`${username}:${password}`);
     
-    // console.log(stringToEncode);
-
     let reponseBody = JSON.stringify({
         "Authorization": "Basic "+base64.encode(stringToEncode)
     });
     
-    // payloadBody = {...payloadBody, message:base64Script};
-
-    // let body = event.body;
-
-    // console.log(JSON.stringify());
-
-
     return {...response, statusCode:200, body: reponseBody}
 
 }
